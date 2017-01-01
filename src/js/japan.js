b85bf4a20ebfd30c00d29219d59edafd75003cf3
Icer <liangshuangde@163.com>
2017-01-01 09:20:59 +0800

    enhance script to support multi accent and accent down-symbol as well

--- src/js/japan.js
+++ src/js/japan.js
@@ -1,10 +1,6 @@
 function japanruby(content) {
   function getChar(sentence, start, len) {
-    if (len == undefined) {
-      len = "*";
-    } else {
-      len = "{" + len + "}"
-    }
+    len = len == undefined ? "*" : ("{" + len + "}");
     var re = new RegExp("(.[ゅょゃュョャ]?){" + start + "}((.[ゅょゃュョャ]?)" + len + ")");
     var result = re.exec(sentence)[2];
     return result;
@@ -13,22 +9,23 @@ function japanruby(content) {
     content = content.replace(/!(.*?)\((.*?)\)/g, '<rt></rt>$1<rt>$2</rt>');
     return '<ruby>' + content + '</ruby>';
   } else if (content.indexOf('@') >= 0) {
-    var re = /(.*)@(\d)/g;
-    var match = re.exec(content);
-    if (!match) return content;
-    var sen = match[1];
-    var num = match[2];
-    if (num == 0) {
-      content = getChar(sen, 0, 1) + "<span class='accent'>" + getChar(sen, 1) + "</span>";
-    } else if (num == 1) {
-      content = "<span class='accent'>" + getChar(sen, 0, 1) + "</span>"
-        + getChar(sen, num);
-    } else {
-      content = getChar(sen, 0, 1)
-        + "<span class='accent'>" + getChar(sen, 1, num - 1) + "</span>"
-        + getChar(sen, num);
-    }
-    return content;
+    // support convert text with accent like `おふろ@2に@はいります@4`
+    var re = /([\u3040-\u309f\u30a0-\u30ff]*)@((?:\d{1,2})?)/g;
+    return content.replace(re, function (_, sen, num) {
+      if (num == undefined || num == "") {
+        return sen;
+      } else if (num == 0) {
+        return getChar(sen, 0, 1)
+          + "<span class='accent0'>" + getChar(sen, 1) + "</span>";
+      } else if (num == 1) {
+        return "<span class='accent'>" + getChar(sen, 0, 1) + "</span>"
+          + getChar(sen, num);
+      } else {
+        return getChar(sen, 0, 1)
+          + "<span class='accent'>" + getChar(sen, 1, num - 1) + "</span>"
+          + getChar(sen, num);
+      }
+    });
   }
   return content;
 }
