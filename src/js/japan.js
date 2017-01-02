f16ac18fe39e1d525937c15cb2332909a0efd308
Icer <liangshuangde@163.com>
2017-01-02 17:00:39 +0800

    fix wrong word and add proper exception handler for this type of error

--- src/js/japan.js
+++ src/js/japan.js
@@ -5,27 +5,32 @@ function japanruby(content) {
     var result = re.exec(sentence)[2];
     return result;
   }
-  if (content.indexOf('!') >= 0) {
-    content = content.replace(/!(.*?)\((.*?)\)/g, '<rt></rt>$1<rt>$2</rt>');
-    return '<ruby>' + content + '</ruby>';
-  } else if (content.indexOf('@') >= 0) {
-    // support convert text with accent like `おふろ@2に@はいります@4`
-    var re = /([\u3040-\u309f\u30a0-\u30ff]*)@((?:\d{1,2})?)/g;
-    return content.replace(re, function (_, sen, num) {
-      if (num == undefined || num == "") {
-        return sen;
-      } else if (num == 0) {
-        return getChar(sen, 0, 1)
-          + "<span class='accent0'>" + getChar(sen, 1) + "</span>";
-      } else if (num == 1) {
-        return "<span class='accent'>" + getChar(sen, 0, 1) + "</span>"
-          + getChar(sen, num);
-      } else {
-        return getChar(sen, 0, 1)
-          + "<span class='accent'>" + getChar(sen, 1, num - 1) + "</span>"
-          + getChar(sen, num);
-      }
-    });
+  try {
+    if (content.indexOf('!') >= 0) {
+      content = content.replace(/!(.*?)\((.*?)\)/g, '<rt></rt>$1<rt>$2</rt>');
+      return '<ruby>' + content + '</ruby>';
+    } else if (content.indexOf('@') >= 0) {
+      // support convert text with accent like `おふろ@2に@はいります@4`
+      var re = /([\u3040-\u309f\u30a0-\u30ff]*)@((?:\d{1,2})?)/g;
+      return content.replace(re, function (_, sen, num) {
+        if (num == undefined || num == "") {
+          return sen;
+        } else if (num == 0) {
+          return getChar(sen, 0, 1)
+            + "<span class='accent0'>" + getChar(sen, 1) + "</span>";
+        } else if (num == 1) {
+          return "<span class='accent'>" + getChar(sen, 0, 1) + "</span>"
+            + getChar(sen, num);
+        } else {
+          return getChar(sen, 0, 1)
+            + "<span class='accent'>" + getChar(sen, 1, num - 1) + "</span>"
+            + getChar(sen, num);
+        }
+      });
+    }
+  }
+  catch(err) {
+    console.error('error parsing "' + content + '"', err);
   }
   return content;
 }
