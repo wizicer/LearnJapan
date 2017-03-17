2c6474dc97515230cb1aeddd42ce5ae1bc4ff92a
Icer <liangshuangde@163.com>
2017-03-18 06:08:35 +0800

    initialize ionic app

---
+++ ionic/src/app/app.component.ts
@@ -0,0 +1,84 @@
+import { Component, ViewChild } from '@angular/core';
+import {Platform, Nav, Config} from 'ionic-angular';
+import { StatusBar, Splashscreen } from 'ionic-native';
+
+import { Settings } from '../providers/providers';
+
+import { FirstRunPage } from '../pages/pages';
+import { CardsPage } from '../pages/cards/cards';
+import { ContentPage } from '../pages/content/content';
+import { LoginPage } from '../pages/login/login';
+import { MapPage } from '../pages/map/map';
+import { SignupPage } from '../pages/signup/signup';
+import { TabsPage } from '../pages/tabs/tabs';
+import { TutorialPage } from '../pages/tutorial/tutorial';
+import { WelcomePage } from '../pages/welcome/welcome';
+import { ListMasterPage } from '../pages/list-master/list-master';
+import { MenuPage } from '../pages/menu/menu';
+import { SettingsPage } from '../pages/settings/settings';
+import { SearchPage } from '../pages/search/search';
+
+import { TranslateService } from 'ng2-translate/ng2-translate';
+
+@Component({
+  template: `<ion-menu [content]="content">
+    <ion-header>
+      <ion-toolbar>
+        <ion-title>Pages</ion-title>
+      </ion-toolbar>
+    </ion-header>
+
+    <ion-content>
+      <ion-list>
+        <button menuClose ion-item *ngFor="let p of pages" (click)="openPage(p)">
+          {{p.title}}
+        </button>
+      </ion-list>
+    </ion-content>
+
+  </ion-menu>
+  <ion-nav #content [root]="rootPage"></ion-nav>`
+})
+export class MyApp {
+  rootPage = FirstRunPage;
+
+  @ViewChild(Nav) nav: Nav;
+
+  pages: any[] = [
+    { title: 'Tutorial', component: TutorialPage },
+    { title: 'Welcome', component: WelcomePage },
+    { title: 'Tabs', component: TabsPage },
+    { title: 'Cards', component: CardsPage },
+    { title: 'Content', component: ContentPage },
+    { title: 'Login', component: LoginPage },
+    { title: 'Signup', component: SignupPage },
+    { title: 'Map', component: MapPage },
+    { title: 'Master Detail', component: ListMasterPage },
+    { title: 'Menu', component: MenuPage },
+    { title: 'Settings', component: SettingsPage },
+    { title: 'Search', component: SearchPage }
+  ]
+
+  constructor(translate: TranslateService, platform: Platform, settings: Settings, config: Config) {
+    // Set the default language for translation strings, and the current language.
+    translate.setDefaultLang('en');
+    translate.use('en')
+
+    translate.get(['BACK_BUTTON_TEXT']).subscribe(values => {
+      config.set('ios', 'backButtonText', values.BACK_BUTTON_TEXT);
+    });
+
+    platform.ready().then(() => {
+      // Okay, so the platform is ready and our plugins are available.
+      // Here you can do any higher level native things you might need.
+      StatusBar.styleDefault();
+      Splashscreen.hide();
+    });
+  }
+
+  openPage(page) {
+    // Reset the content nav to have just this page
+    // we wouldn't want the back button to show in this scenario
+    this.nav.setRoot(page.component);
+  }
+}
---
+++ ionic/src/pages/item-detail/item-detail.ts
@@ -0,0 +1,17 @@
+import { Component } from '@angular/core';
+import { NavController, NavParams } from 'ionic-angular';
+
+import { Items } from '../../providers/providers';
+
+@Component({
+  selector: 'page-item-detail',
+  templateUrl: 'item-detail.html'
+})
+export class ItemDetailPage {
+  item: any;
+
+  constructor(public navCtrl: NavController, navParams: NavParams, items: Items) {
+    this.item = navParams.get('item') || items.defaultItem;
+  }
+
+}
